82c7e8ae73b50332860ce731feda80b46082761e
Freddie Bryant <freddie.bryantk4@gmail.com>
2026-01-29 13:03:05 -0500

    Practice showPrime numbers function

--- index.js
+++ index.js
@@ -49,17 +49,15 @@
 
 showPrime(20);
 
-function showPrime(limit) {
-  for (let number = 2; number <= limit; number++) {
+function showPrime(limit){
+  for (let number = 2; number <= limit; number++)
     if (isPrime(number)) console.log(number);
-  }
 }
 
 function isPrime(number) {
-    for (let factor = 2; factor < number; factor++)
-      if (number % factor === 0)
-        return false;
+    for (let factor = 2; factor < number; factor++) 
+      if (number % factor === 0) 
+       return false;       
     
     return true;
 }
-
